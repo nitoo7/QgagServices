0010368818ed82c281c6257959e4a7c025c115f2
user <nithint@qburst.com>
2016-10-04 19:11:48 +0530

    connection succesful to mongodb

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ const _ = require('lodash');
 
 var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/example');
-mongoose.connect('mongodb://nitoo7:manchester007@ds051585.mongolab.com:51585/sampletestdb');
+// mongoose.connect('mongodb://nitoo7:manchester007@ds051585.mongolab.com:51585/sampletestdb');
+mongoose.connect('mongodb://admin:password@ds049436.mlab.com:49436/qgag_services');
 
 var Schema = mongoose.Schema;
-var movieSchema = new Schema({
+var gagSchema = new Schema({
  title: String,
-  year: Number,
-  director: String,
-  rating: String
+ image: String
 });
-var Movie = mongoose.model('movie', movieSchema);
+var Gag = mongoose.model('gag', gagSchema);
 
 
 app.get('/', function (req, res) {
@@ -23,7 +22,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/details', function (req, res) {
-  Movie.findOne({}).exec().then(function(data){return res.send(data)})
+  Gag.findOne({}).exec().then(function(data){return res.send(data)})
 });
 
 app.listen(process.env.PORT || 3000, function () {
